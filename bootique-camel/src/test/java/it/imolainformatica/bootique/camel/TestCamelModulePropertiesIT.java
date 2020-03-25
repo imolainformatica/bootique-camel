0f6cfe84c5e6894df3dd7c57460192795e719bf6
Giovanni Cuccu <gcuccu@imolainformatica.it>
2020-03-25 08:05:44 +0100

    -aadded configuration for useMDCLogging and allowUseOriginalMessage

---
+++ bootique-camel/src/test/java/it/imolainformatica/bootique/camel/TestCamelModulePropertiesIT.java
@@ -0,0 +1,43 @@
+package it.imolainformatica.bootique.camel;
+
+import io.bootique.BQRuntime;
+import io.bootique.di.BQModule;
+import io.bootique.test.junit.BQTestFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TestCamelModuleIT {
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory().autoLoadModules();
+
+    private BQRuntime startApp(BQModule module) {
+        BQRuntime runtime = testFactory.app("-s").args("--config=classpath:bootique.yml")
+                .module(module)
+                .createRuntime();
+        runtime.run();
+
+        return runtime;
+    }
+
+    @Test
+    public void testSimpleBuilder() throws InterruptedException {
+        BQRuntime runtime=startApp(b ->CamelModule.extend(b).addRouteBuilder(new RouteBuilder() {
+            public void configure() {
+                from("direct:start").streamCaching().to("mock:result");
+            }
+        }));
+        CamelContext camelContext= runtime.getInstance(CamelContext.class);
+        Assert.assertNotNull(camelContext);
+        ProducerTemplate producerTemplate=camelContext.createProducerTemplate();
+
+        MockEndpoint mockEndpoint = (MockEndpoint) camelContext.getEndpoint("mock:result");
+        producerTemplate.sendBody("direct:start","ciao");
+        mockEndpoint.setExpectedMessageCount(1);
+        mockEndpoint.assertIsSatisfied();
+    }
+}
